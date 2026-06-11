2b59bfd4cfaea43c7ab08f01083c8b6446ecd216
Pavani Dangudubiyyam <24b01a1227@svecw.edu.in>
2026-06-11 18:11:53 +0530

    Created FAISS vector database

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
+# Load PDF
 loader = PyPDFLoader(
     "data/IPL_LangGraph_RAG_Dataset.pdf"
 )
 
 docs = loader.load()
 
+# Split into chunks
 splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=200
@@ -14,4 +18,18 @@ splitter = RecursiveCharacterTextSplitter(
 
 chunks = splitter.split_documents(docs)
 
-print("Total Chunks:", len(chunks))
+# Create embeddings
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# Create FAISS DB
+db = FAISS.from_documents(
+    chunks,
+    embeddings
+)
+
+# Save DB
+db.save_local("faiss_db")
+
+print("FAISS Vector Database Created Successfully")
